b4c27a3190517fc646dfd7313c5a8e1f9ecb1367
João Amaro Lagedo <jlagedo@gmail.com>
2025-12-13 22:08:05 -0300

    feat: add CORS policy to allow Angular frontend communication
    Configure CORS middleware with "AllowAngular" policy allowing requests from http://localhost:4200 with any headers and methods, and apply the policy in the request pipeline before controller mapping.

--- Program.cs
+++ Program.cs
@@ -17,8 +17,18 @@ builder.Services.AddDbContext<ToDoDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
+app.UseCors("AllowAngular");
 app.MapControllers();
 
 app.Run();
